f7dcf0e31dcc76d6f4e87204c1a229a6249ecbe2
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-11-08 18:45:00 -0400

    feat: configure email provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,16 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-// import DiscordProvider from "next-auth/providers/discord";
-
-// Prisma adapter for NextAuth, optional and can be removed
+import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
-// import { env } from "../../../env/server.mjs";
+import { env } from "../../../env/server.mjs";
+import nodemailer, { type TransportOptions } from "nodemailer";
+import { smtpConfig } from "@/lib/smtpConfig";
+import { html, text } from "@/lib/emailTemplate";
+
+const transporter = nodemailer.createTransport<TransportOptions>(
+  smtpConfig.transport as TransportOptions
+);
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -16,15 +22,31 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    // DiscordProvider({
-    // clientId: env.DISCORD_CLIENT_ID,
-    // clientSecret: env.DISCORD_CLIENT_SECRET,
-    // }),
-    // ...add more providers here
+    EmailProvider({
+      type: "email",
+      // @INFO: Magic links are valid for 10 minutes.
+      maxAge: 10 * 60 * 60,
+      sendVerificationRequest: ({ identifier, url, provider, theme }) => {
+        const { host } = new URL(url);
+        console.log(identifier, "identifier");
+        console.log(url, "url");
+        console.log("provider: ", provider);
+        console.log("theme: ", theme);
+        transporter.sendMail({
+          from: `${env.EMAIL_FROM}` || "ntornos.com",
+          to: identifier,
+          subject: "Your sign-in link for ntornos.com",
+          text: text({ url, host }),
+          html: html({ url, host, theme }),
+        });
+      },
+    }),
   ],
+  // pages: {
+  //   signIn: "/auth/signin",
+  // },
 };
 
 export default NextAuth(authOptions);
