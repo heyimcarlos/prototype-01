cba7f6f0853aeed17ae5ae3b110b82a92abc19f2
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-11-08 21:32:14 -0400

    chore: add GoogleProvider

--- src/lib/smtpConfig.ts
+++ src/lib/smtpConfig.ts
@@ -1,5 +1,5 @@
-import SendmailTransport from "nodemailer/lib/sendmail-transport";
-import SMTPConnection from "nodemailer/lib/smtp-connection";
+import type SendmailTransport from "nodemailer/lib/sendmail-transport";
+import type SMTPConnection from "nodemailer/lib/smtp-connection";
 import { env } from "@/env/server.mjs";
 
 function detectTransport():
