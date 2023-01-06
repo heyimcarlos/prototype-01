b9fa4ec6fc0bb1391967451a40fe444a800baddf
heyimcarlos <me@heyimcarlos.dev>
2023-01-06 17:25:19 -0500

    chore: create new map filter form component to isolate filters form implementation, also initiate form

--- src/components/FlyoutMenu.tsx
+++ src/components/FlyoutMenu.tsx
@@ -1,46 +1,48 @@
-import React, { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
+import React, { useCallback, useState } from "react";
+import * as Popover from "@radix-ui/react-popover";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 import classNames from "@/lib/classNames";
 
-const FlyoutMenu: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <Popover className="relative">
-      {({ open }) => (
-        <>
-          <Popover.Button
-            className={classNames(
-              open ? "text-gray-900 " : "text-gray-500 ",
-              "group border p-1 inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
-            )}
-          >
-            <span className="mx-2">Preferences</span>
-            <ChevronDownIcon
-              className={classNames(
-                open ? "text-gray-600" : "text-gray-400",
-                "ml-2 h-5 w-5 group-hover:text-gray-500"
-              )}
-              aria-hidden="true"
-            />
-          </Popover.Button>
+type Props = {
+  children: React.ReactNode;
+} & (
+    | {
+      triggerIcon: JSX.Element;
+      triggerText?: never;
+    }
+    | {
+      triggerIcon?: never;
+      triggerText: string;
+    }
+  );
 
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel className="absolute left-40 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0">
-              {children}
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
-    </Popover>
+const FlyoutMenu = ({ children, triggerIcon, triggerText }: Props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = (open: boolean) => setOpen(open);
+
+  const trigger = triggerIcon ?? (
+    <button className="group inline-flex items-center rounded-md border bg-white p-1 text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
+      <span className="mx-2">{triggerText}</span>
+      <ChevronDownIcon
+        className={classNames(
+          open ? "text-gray-600" : "text-gray-400",
+          "ml-2 h-5 w-5 group-hover:text-gray-500"
+        )}
+        aria-hidden="true"
+      />
+    </button>
+  );
+
+  return (
+    <Popover.Root open={open} onOpenChange={handleOpen}>
+      <Popover.Trigger asChild className="cursor-pointer">{trigger}</Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content className="z-10 m-2 w-32 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {children}
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 };
 
--- src/components/mapPageComponents/topNavbars/MapTopbar.tsx
+++ src/components/mapPageComponents/topNavbars/MapTopbar.tsx
@@ -1,14 +1,38 @@
-// import PreferenceForm from "./PreferenceForm";
 import { useNeighborhoods } from "@/stores/useNeighborhoods";
 import SectorsSelected from "./SectorsSelected";
-// import useWindowSize from "@/hooks/useWindowSize";
 import FiltersFlyoutMenu from "./FiltersFlyoutMenu";
 import { type Dispatch, type SetStateAction, useState } from "react";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import FlyoutMenu from "@/components/FlyoutMenu";
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/20/solid";
+import { ListingType, PropertyType } from "@prisma/client";
 
-const MapTopbar = () => {
-  const neighborhoods = useNeighborhoods((state) => state.neighborhoods);
-  // const width = useWindowSize();
+const FormSchema = z.object({
+  name: z.string().min(1, "required"),
+  minPrice: z.number().min(0),
+  maxPrice: z.number().max(30000000),
+  bedrooms: z.number(),
+  bathrooms: z.number(),
+  halfBathrooms: z.number(),
+  listingType: z.nativeEnum(ListingType),
+  propertyType: z.nativeEnum(PropertyType)
+});
+type FormValues = z.infer<typeof FormSchema>;
+
+const MapFiltersForm = () => {
+  const {
+    register,
+    handleSubmit,
+  } = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (data: FormValues) => {
+    console.log('data', data)
+  }
 
   const { query } = useRouter();
 
@@ -31,17 +55,58 @@ const MapTopbar = () => {
     query.listingType ? query.listingType : "For Sale & Rent"
   );
 
-  // const neighborhoods = await prisma.neighborhood.findMany({
-  //   include: {
-  //     listingLocations: { include: { listings: {}, neighborhood: {} } },
-  //   },
-  // });
+  return (
+    <>
+      <FlyoutMenu triggerIcon={<AdjustmentsHorizontalIcon className="h-6 w-6" />}>
+        <form onSubmit={handleSubmit(onSubmit)}>
 
-  // const getFilteredListings = async () => {
-  //   const getFilteredListings = await prisma?.listing.findMany({
-  //     where: { price: { gte: minValue, lte: maxValue } },
-  //   });
-  // };
+          <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Name
+            </label>
+            <div className="mt-1 sm:col-span-2 sm:mt-0">
+              <input
+                {...register("name")}
+                type="text"
+                placeholder="Maria"
+                name="name"
+                id="name"
+                autoComplete="given-name"
+                className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
+              />
+              {/*
+                      {errors.name?.message && (
+                        <ErrorMessage message={errors.name.message} />
+                      )}
+                      */}
+            </div>
+          </div>
+          <button type="submit">apply</button>
+        </form>
+      </FlyoutMenu>
+      <FiltersFlyoutMenu
+        minPrice={minPrice}
+        setMinPrice={setMinPrice}
+        maxPrice={maxPrice}
+        setMaxPrice={setMaxPrice}
+        bedrooms={bedrooms}
+        setBedrooms={setBedrooms}
+        fullBathrooms={fullBathrooms}
+        setFullBathrooms={setFullBathrooms}
+        halfBathrooms={halfBathrooms}
+        setHalfBathrooms={setHalfBathrooms}
+        listingType={listingType as string}
+        setListingType={setListingType as Dispatch<SetStateAction<string>>}
+      />
+    </>
+  );
+};
+
+const MapTopbar = () => {
+  const neighborhoods = useNeighborhoods((state) => state.neighborhoods);
 
   return (
     <div className="h-[2.1rem] w-full bg-indigo-600">
@@ -50,20 +115,7 @@ const MapTopbar = () => {
       </div>
 
       <div className="fixed right-0 z-[51] mr-1.5 pt-[0.2rem]">
-        <FiltersFlyoutMenu
-          minPrice={minPrice}
-          setMinPrice={setMinPrice}
-          maxPrice={maxPrice}
-          setMaxPrice={setMaxPrice}
-          bedrooms={bedrooms}
-          setBedrooms={setBedrooms}
-          fullBathrooms={fullBathrooms}
-          setFullBathrooms={setFullBathrooms}
-          halfBathrooms={halfBathrooms}
-          setHalfBathrooms={setHalfBathrooms}
-          listingType={listingType as string}
-          setListingType={setListingType as Dispatch<SetStateAction<string>>}
-        />
+        <MapFiltersForm />
       </div>
     </div>
   );
